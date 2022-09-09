72de3e254cf25e47b97f11ef23a4650a04f94bb7
Aggie05 <agnesoluwabusola@gmail.com>
2022-09-09 12:43:25 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Return: Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -15,3 +15,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
+}
