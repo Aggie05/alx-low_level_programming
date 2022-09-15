cabfec60c22bade5cc6877964b18dca518993d75
Aggie05 <agnesoluwabusola@gmail.com>
2022-09-15 03:55:49 -0700

    1-isdigit

---
+++ 1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _isdigit -  checks for a digit (0 through 9)
+ * @c: the digit
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c);
+{
+	
+	if (c >= 48 && c <= 57)
+	{
+		return (0);
+	}
+	
+	return (0);
+}
