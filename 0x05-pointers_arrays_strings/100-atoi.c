4d67fe7809cb845a55a891935829d631226e2174
Aggie05 <agnesoluwabusola@gmail.com>
2022-09-19 20:20:44 -0700

    task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,18 +19,18 @@ int _atoi(char *s)
 {
 	unsigned int number, i;
 	int sign;
-	
+
 	sign = 1;
 	number = 0;
-	
-	
-	
+
+
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ( is_numerical(s[i]))
+		if (is_numerical(s[i]))
 		{
 			number = (s[i] - 48) + number * 10;
-			
+
 			if (s[i + 1] == ' ')
 				break;
 		}
@@ -38,9 +38,9 @@ int _atoi(char *s)
 		{
 			sign *= -1;
 		}
-	
+
 	}
-	
+
 	return (number *sign);
 
 
