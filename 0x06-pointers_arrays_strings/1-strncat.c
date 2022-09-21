8c984f5d408e77da7a30880a91787707eca5ba7a
Aggie05 <agnesoluwabusola@gmail.com>
2022-09-21 14:41:42 -0700

    fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,11 +4,11 @@
 /**
  * _strncat - function to append some charx
  * @dest: first parameter
- * @src: : second parameter
+ * @src: second parameter
  * @n: 3rd parameter
  * Return: string
  */
-char *_strncat(char *dst, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	strncat(dest, src, n);
 	return (dest);
